2008f52e438ea03ecbb30abd4f73577ceb114ed3
antikmozib <45801225+antikmozib@users.noreply.github.com>
2020-09-04 00:40:33 +1000

    fixed untracked files

--- Setup/Output/sign.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool\signtool.exe" sign /f dccs.pfx /p hdnujf7693kg7 /t http://timestamp.verisign.com/scripts/timstamp.dll dupeclear1.84_setup.exe

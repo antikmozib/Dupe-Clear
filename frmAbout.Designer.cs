07045f2b6a529ef98f58b2dd4c4c361dd9ac73ac
Antik Mozib <45801225+antikmozib@users.noreply.github.com>
2023-06-21 00:22:17 +1000

    Update icon

--- frmAbout.Designer.cs
+++ frmAbout.Designer.cs
@@ -48,7 +48,7 @@
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(8, 8);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(75, 74);
+            this.pictureBox1.Size = new System.Drawing.Size(75, 75);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
